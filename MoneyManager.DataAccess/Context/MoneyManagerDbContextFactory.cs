772687f8b63e8e500b214754e4bbeb1a24870a54
Ivan <neborsky.ivan@tut.by>
2026-04-10 12:18:01 +0300

    feat: add design time db context factory

---
+++ MoneyManager.DataAccess/Context/MoneyManagerDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MoneyManager.DataAccess.Context;
+
+public class MoneyManagerDbContextFactory : IDesignTimeDbContextFactory<MoneyManagerDbContext>
+{
+    public MoneyManagerDbContext CreateDbContext(string[] args)
+    {
+        var projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\.."));
+        var configPath = Path.Combine(projectRoot, "appsettings.json");
+        IConfiguration config = new ConfigurationBuilder().AddJsonFile(configPath).Build();
+        var connectionString = config.GetConnectionString("SqlServerConnectionString");
+        var optionsBuilder = new DbContextOptionsBuilder<MoneyManagerDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+        
+        return new MoneyManagerDbContext(optionsBuilder.Options);
+    }
+}
